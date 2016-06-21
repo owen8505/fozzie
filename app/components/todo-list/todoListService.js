3123ebc93050627737858e13c4cdccb790a9ea28
Luis Antonio Sánchez <luis.sanchez.franco@gmail.com>
2016-06-21 16:47:49 -0500

    Se agregaron estilos para las fechas con warning

--- app/components/todo-list/todoListService.js
+++ app/components/todo-list/todoListService.js
@@ -4,11 +4,11 @@
     angular.module('cftest').factory('todoListService', ['$http', function ($http) {
 
         var tasks = [
-            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-15T21:28:42.606Z'), completedDate: undefined },
-            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-21T21:28:42.606Z'), completedDate: undefined },
-            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-22T21:28:42.606Z'), completedDate: undefined },
-            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-14T21:28:42.606Z'), completedDate: undefined },
-            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-23T21:28:42.606Z'), completedDate: undefined }
+            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-15T21:28:42.606Z'), completedDate: undefined, order_info: {} },
+            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-21T21:28:42.606Z'), completedDate: undefined, order_info: {} },
+            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-22T21:28:42.606Z'), completedDate: undefined, order_info: {} },
+            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-14T21:28:42.606Z'), completedDate: undefined, order_info: {} },
+            { id: 1, description: 'Recolección Juan Martínez', completed: false, dueDate: new moment('2016-06-23T21:28:42.606Z'), completedDate: undefined, order_info: {} }
         ];
 
         function getTasks() {
