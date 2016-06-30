aafb469266cf19ca6b13885356d02dfb3ef445b7
Luis Antonio Sánchez <luis.sanchez.franco@gmail.com>
2016-06-30 16:42:43 -0500

    Se agregaron estilos al login

---
+++ app/shared/services/routing.service.js
@@ -0,0 +1,8 @@
+(function(angular) {
+    'use strict';
+
+    angular.module('dispatcher').factory('routingService', ['$http', '$rootScope', function ($http, $rootScope) {
+
+    }]);
+
+})(window.angular);
